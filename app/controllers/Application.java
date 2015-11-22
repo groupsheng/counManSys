fd343fd67f4221f1ab3872d4cbd730b712a5b98b
王玺 <1064393425@qq.com>
2015-11-22 16:32:32 +0800

    新建工程

---
+++ app/controllers/Application.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+	// github 测
+    public static void index() {
+
+    	render();
+    }
+    
+    public static void cunmin() {
+
+    	render();
+    }
+    
+    public static void cunqing() {
+
+    	render();
+    }
+
+}
